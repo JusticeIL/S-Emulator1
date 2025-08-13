212d56c7cb9dababf0d3ab66e20933af8c21c0c1
JusticeIL <galru3@mta.ac.il>
2025-08-13 14:57:15 +0300

    Created variable ctor

--- src/Engine/Variable.java
+++ src/Engine/Variable.java
@@ -7,6 +7,12 @@ public class Variable {
     private int id;
     private static int highestUsedID;
 
+    public Variable(String name, int value) {
+        this.value = value;
+        this.name = name;
+        this.id = ++highestUsedID;
+    }
+
     public int getValue() {
         return value;
     }
