1d23054b42229af974c6ec5478f609a456ef37a4
JusticeIL <galru3@mta.ac.il>
2025-08-21 19:30:24 +0300

    Created the console ui class and handled option 1 and 2

--- Engine/src/controller/Controller.java
+++ Engine/src/controller/Controller.java
@@ -8,11 +8,11 @@ import instruction.component.Variable;
 
 import java.io.FileNotFoundException;
 import java.util.Collection;
+import java.util.Optional;
 
 public interface Controller {
-    Program getProgram();
-    ControllerResponse loadProgram(String path) throws JAXBException, FileNotFoundException;
-    ProgramData getProgramData(Program program);
+    void loadProgram(String path) throws JAXBException, FileNotFoundException;
+    Optional<ProgramData> getProgramData();
     Void Expand(int level);
     Collection<Variable> RunProgram();
     Statistics getStatistics();
