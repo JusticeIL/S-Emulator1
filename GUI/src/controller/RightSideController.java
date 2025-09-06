2008aa0274b826c8daf1c03bd36a5f8ec254cdf7
EitanVeryKatz <eitanka@mta.ac.il>
2025-09-06 19:27:06 +0300

    Created SkeletonController for Right side

---
+++ GUI/src/controller/RightSideController.java
@@ -0,0 +1,89 @@
+package controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+public class RightSideController extends  BorderPane {
+
+    @FXML
+    private Button AddArgumentBtn;
+
+    @FXML
+    private TableView<?> ExecutionArgumentInput;
+
+    @FXML
+    private Button RerunBtn;
+
+    @FXML
+    private Button ResumeDebugBtn;
+
+    @FXML
+    private Button RunProgramBtn;
+
+    @FXML
+    private Button ShowStatisticsBtn;
+
+    @FXML
+    private Button StartDebugBtn;
+
+    @FXML
+    private TableView<?> StatisticsTable;
+
+    @FXML
+    private Button StepOverDebugBtn;
+
+    @FXML
+    private Button StopDebugBtn;
+
+    @FXML
+    private TableView<?> VariableTable;
+
+    @FXML
+    void AddArgumentPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ExecutionArgumentUpdated(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RerunPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ResumeDebugPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RunProgramPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ShowStatisticsPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void StartDebugPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void StepOverDebugPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void StopDebugPressed(ActionEvent event) {
+
+    }
+
+}
