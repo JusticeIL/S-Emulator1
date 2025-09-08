51e6e2cc798f9eccb60cdb2d93456965bc1ceed9
JusticeIL <galru3@mta.ac.il>
2025-09-08 17:24:27 +0300

    - Started writing the file chooser in the TopComponentController

--- GUI/src/controller/TopComponentController.java
+++ GUI/src/controller/TopComponentController.java
@@ -6,10 +6,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class TopComponentController extends HBox {
 
     private BorderPane mainController;
+    private Stage primaryStage;
 
     public void setMainController(BorderPane mainController) {
         this.mainController = mainController;
@@ -23,6 +28,14 @@ public class TopComponentController extends HBox {
 
     @FXML
     void loadProgramPressed(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Program");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Program File", "*.xml"));
+        File selectedFile = fileChooser.showOpenDialog(primaryStage);
+        if (selectedFile == null) {
+            return;
+        }
 
+        String absolutePath = selectedFile.getAbsolutePath();
     }
 }
