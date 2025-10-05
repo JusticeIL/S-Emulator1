f0b604e28a873d19be4ea9ad163de96ad00c90c0
EitanVeryKatz <eitanka@mta.ac.il>
2025-10-05 20:39:10 +0300

    passed test for loading program via path in body

--- Server/src/ProgramServlet.java
+++ Server/src/ProgramServlet.java
@@ -15,20 +15,28 @@ public class ProgramServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Model model = (Model) getServletContext().getContext("model");
+        Model model = (Model) getServletContext().getAttribute("model");
         Optional<ProgramData> data = model.getProgramData();
         resp.setContentType("text/html");
-        if (data.isPresent()) {
-            resp.getWriter().write("<html><body><h2>Get Program Succeeded.</h2></body></html>");
-        } else {
-            resp.getWriter().write("<html><body><h2>No program loaded.</h2></body></html>");
-        }
+        data.ifPresent(programData -> {
+            try {
+                resp.getWriter().write("Program Name: " + programData.getProgramName() + "\n");
+            } catch (IOException e) {
+                try {
+                    resp.getWriter().write("no program loaded yet");
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String sprogramPath = req.getParameter("path");
-        Model model = (Model) getServletContext().getContext("model");
+        //Expects the body to contain the path to the program xml file
+        
+        String sprogramPath = req.getReader().readLine();
+        Model model = (Model) getServletContext().getAttribute("model");
         try {
             model.loadProgram(sprogramPath);
             doGet(req, resp);
@@ -36,4 +44,5 @@ public class ProgramServlet extends HttpServlet {
             resp.getWriter().write("failed to load program: " + e.getMessage());
         }
     }
+
 }
