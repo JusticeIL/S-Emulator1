9e7fcab9233c98ffeb2f68ba407436c8d0e8ea8d
JusticeIL <galru3@mta.ac.il>
2025-10-17 12:41:04 +0300

    - Removed addHeader method of the cookies and instead used the implementation of Aviad to the cookie jar in the SimpleCookieManager class - Added the refresh rate constant to the client configuration (maybe will open another class for constants or common later)

--- Client/src/dashboard/refreshTasks/UserListRefresher.java
+++ Client/src/dashboard/refreshTasks/UserListRefresher.java
@@ -14,81 +14,78 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.*;
 
-public class UserListRefresher extends TimerTask {
-    private OkHttpClient client;
-    private TableView<UserTableEntry> usersTable;
-    private final String BASE_URL = "http://localhost:8080/S-emulator";
-    private final String RESOURCE = "/api/users";
-    private final int REFRESH_RATE = 1500; // in milliseconds
+import static configuration.ClientConfiguration.CLIENT;
+import static configuration.ResourcesConfiguration.BASE_URL;
+import static configuration.ResourcesConfiguration.GET_ALL_USERS_RESOURCE;
 
-    public UserListRefresher(OkHttpClient client, TableView<UserTableEntry> table) {
-        this.client = client;
+public class UserListRefresher extends TimerTask {
+    private TableView<UserTableEntry> usersTable;
+
+    public UserListRefresher(TableView<UserTableEntry> table) {
         this.usersTable = table;
     }
 
     @Override
     public void run() {
-        if (client != null) {
-            HttpUrl.Builder urlBuilder = Objects.requireNonNull(HttpUrl.parse(BASE_URL + RESOURCE))
-                    .newBuilder();
-            String finalURL = urlBuilder.build().toString();
+        HttpUrl.Builder urlBuilder = Objects.requireNonNull(HttpUrl.parse(BASE_URL + GET_ALL_USERS_RESOURCE))
+                .newBuilder();
+        String finalURL = urlBuilder.build().toString();
 
-            Request request = new Request.Builder()
-                    .url(finalURL)
-                    .get()
-                    .build();
+        Request request = new Request.Builder()
+                .url(finalURL)
+                .get()
+                .build();
 
-            Call call = client.newCall(request);
-            call.enqueue(new Callback() {
-                @Override
-                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+        Call call = CLIENT.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
 
-                    try (ResponseBody body = response.body()) {
-                        if (response.isSuccessful()) {
-                            Gson gson = new Gson();
-                            String responseBody = Objects.requireNonNull(body).string();
-                            Type type = new TypeToken<Set<UserDTO>>() {
-                            }.getType();
-                            Set<UserDTO> users = gson.fromJson(responseBody, type);
-                            List<UserTableEntry> fetchedUsers = users.stream()
-                                    .map(UserTableEntry::new)
-                                    .sorted() // Sorted users by lexicographical order of usernames
-                                    .toList();
+                try (ResponseBody body = response.body()) {
+                    if (response.isSuccessful()) {
+                        Gson gson = new Gson();
+                        String responseBody = Objects.requireNonNull(body).string();
+                        Type type = new TypeToken<Set<UserDTO>>() {
+                        }.getType();
+                        Set<UserDTO> users = gson.fromJson(responseBody, type);
+                        List<UserTableEntry> fetchedUsers = users.stream()
+                                .map(UserTableEntry::new)
+                                .sorted() // Sorted users by lexicographical order of usernames
+                                .toList();
 
-                            Platform.runLater(() -> {
-                                ObservableList<UserTableEntry> currentUsers = usersTable.getItems();
-                                Map<String, Integer> usernameToIndex = new HashMap<>();
-                                for (int i = 0; i < currentUsers.size(); i++) {
-                                    usernameToIndex.put(currentUsers.get(i).getUsername(), i);
-                                }
+                        Platform.runLater(() -> {
+                            ObservableList<UserTableEntry> currentUsers = usersTable.getItems();
+                            Map<String, Integer> usernameToIndex = new HashMap<>();
+                            for (int i = 0; i < currentUsers.size(); i++) {
+                                usernameToIndex.put(currentUsers.get(i).getUsername(), i);
+                            }
 
-                                for (UserTableEntry fetchedUser : fetchedUsers) {
-                                    String username = fetchedUser.getUsername();
-                                    Integer idx = usernameToIndex.get(username);
-                                    if (idx != null) {
-                                        UserTableEntry currentUser = currentUsers.get(idx);
-                                        if (!fetchedUser.equals(currentUser)) {
-                                            currentUsers.set(idx, fetchedUser);
-                                        }
-                                    } else {
-                                        currentUsers.add(fetchedUser);
+                            for (UserTableEntry fetchedUser : fetchedUsers) {
+                                String username = fetchedUser.getUsername();
+                                Integer idx = usernameToIndex.get(username);
+                                if (idx != null) {
+                                    UserTableEntry currentUser = currentUsers.get(idx);
+                                    if (!fetchedUser.equals(currentUser)) {
+                                        currentUsers.set(idx, fetchedUser);
                                     }
+                                } else {
+                                    currentUsers.add(fetchedUser);
                                 }
-                            });
+                            }
+                        });
 
-                        } else { //TODO: handle error by creating a new dialog window
-                            System.out.println("Failed to fetch user list: " + response.code());
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    } else { //TODO: handle error by creating a new dialog window
+                        System.out.println("Failed to fetch user list: " + response.code());
                     }
-                }
-
-                @Override
-                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
-            });
-        }
+            }
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
