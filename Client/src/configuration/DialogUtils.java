4f3742423a821896bcdfb37019b5a86643429e05
JusticeIL <galru3@mta.ac.il>
2025-10-19 15:05:44 +0300

    - Moved the show error dialog method to a dedicated class so it is known to all client packages and other classes as well.

--- Client/src/configuration/DialogUtils.java
+++ Client/src/configuration/DialogUtils.java
@@ -1,4 +1,27 @@
 package configuration;
 
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 public class DialogUtils {
+    public static void showAlert(String message, Stage primaryStage) {
+        Platform.runLater(() -> {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            Button okButton = (Button) alert.getDialogPane().lookupButton(ButtonType.OK); // Helps access and style the ok button
+            okButton.getStyleClass().add("ok-button-error");
+            alert.initOwner(primaryStage);
+            alert.initModality(Modality.APPLICATION_MODAL);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error occurred");
+            alert.setContentText(message);
+
+            // Ensure the dialog is focused
+            alert.setOnShown(dialogEvent -> alert.getDialogPane().requestFocus());
+            alert.showAndWait();
+        });
+    }
 }
--- Client/src/login/controller/MainController.java
+++ Client/src/login/controller/MainController.java
@@ -9,13 +9,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import okhttp3.*;
 
 import java.util.Objects;
 
 import static configuration.ClientConfiguration.CLIENT;
+import static configuration.DialogUtils.showAlert;
 import static configuration.ResourcesConfiguration.BASE_URL;
 import static configuration.ResourcesConfiguration.USER_RESOURCE;
 
@@ -89,23 +89,5 @@ public class MainController {
                     showAlert("Error: " + e.getMessage(), primaryStage);
                 }
             }).start();
-    }
-
-    //TODO: Refactor this method to a utility class if needed in other controllers
-    public static void showAlert(String message, Stage primaryStage) {
-        Platform.runLater(() -> {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            Button okButton = (Button) alert.getDialogPane().lookupButton(ButtonType.OK); // Helps access and style the ok button
-            okButton.getStyleClass().add("ok-button");
-            alert.initOwner(primaryStage);
-            alert.initModality(Modality.APPLICATION_MODAL);
-            alert.setTitle("Error");
-            alert.setHeaderText("Error occurred");
-            alert.setContentText(message);
-
-            // Ensure the dialog is focused
-            alert.setOnShown(dialogEvent -> alert.getDialogPane().requestFocus());
-            alert.showAndWait();
-        });
     }
 }
